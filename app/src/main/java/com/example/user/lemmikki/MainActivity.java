ecd41cf8bcf1be9ea2115dcefcd99c354f87f3d1
ToveVirtanen <yaoshica@gmail.com>
2017-06-07 14:47:52 +0300

    Это самая последняя и самая неработающая версия.

--- app/src/main/java/com/example/user/lemmikki/MainActivity.java
+++ app/src/main/java/com/example/user/lemmikki/MainActivity.java
@@ -7,14 +7,21 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import static com.example.user.lemmikki.R.id.EditText1;
+import static com.example.user.lemmikki.R.id.button6;
+import static com.example.user.lemmikki.R.id.main_container;
 
 public class MainActivity extends AppCompatActivity {
 
     Handler handler = new Handler();
     int BackGroundColorCurr;
+    int CheckerEditText1 = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -97,23 +104,30 @@ public class MainActivity extends AppCompatActivity {
         };
         button4.setOnClickListener(listener4);
 
-        Button button6 = (Button) findViewById(R.id.button6);
+        final Button button6 = (Button) findViewById(R.id.button6);
         View.OnClickListener listener6 = new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (BackGroundColorCurr == 0) {
-                    imageView.setImageResource(R.drawable.stars);
-                    handler.postDelayed(new Runnable() {
-                        @Override
-                        public void run() {
-                            imageView.setImageResource(R.drawable.egg);
-                        }
-                    }, 1000);
+                TextView textView3 = (TextView) findViewById(R.id.TextView3);
+                EditText editText1 = (EditText) findViewById(R.id.EditText1);
+                switch (CheckerEditText1) {
+                    case 0:
+                        button6.setText("OK");
+                        textView3.setVisibility(View.INVISIBLE);
+                        editText1.setVisibility(View.VISIBLE);
+                        CheckerEditText1 = 1;
+                        break;
+                    case 1:
+                        button6.setText("Change name");
+                        textView3.setText(editText1.getText().toString());
+                        editText1.setVisibility(View.INVISIBLE);
+                        textView3.setVisibility(View.VISIBLE);
+                        CheckerEditText1 = 0;
+                        break;
+                    }
                 }
-            }
         };
         button6.setOnClickListener(listener6);
-
     }
 
 }
